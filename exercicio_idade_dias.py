2c3fcf62ecddc1ae1ea5075a6c6a7ad7c85914e2
Lillyan <103754183+Liillyan@users.noreply.github.com>
2023-05-04 10:27:46 -0300

    Update exercicio_idade_dias.py
    Alteração solicitada pelo professor, onde deveríamos alterar o código para que o usuário consiga repetir a aplicação caso queira, foi utilizado while para que a estrutura de repetição fosse criado.

--- exercicio_idade_dias.py
+++ exercicio_idade_dias.py
@@ -1,13 +1,20 @@
 ''' Faça um algoritimo que leia a idade de uma pessoa
 expressa em anos, meses e dias e escreva a idade dessa 
-pessoa expressa em apenas dias. considere o ano com 365 diase o mês com 30 dias'''
+pessoa expressa em apenas dias. considere o ano com 365 diase o mês com 30 dias
+Altere o código para que o usuario possa repetir a aplicação caso queira.
+'''
 
-idade_ano = int(input("Digite aqui o sua idade: "))
-idade_mes = int(input("Digite aqui quantos meses voce tem: "))
-idade_dia = int(input("Digite aqui quantos dias voce tem: "))
+continua = "sim"
 
-ano = idade_ano*365
-mes = idade_mes*30
-soma = ano+mes+idade_dia
-print("Sua idade em dias é ", soma)
+while continua == "sim":
+    idade_ano = int(input("Digite aqui o sua idade: "))
+    idade_mes = int(input("Digite aqui quantos meses voce tem: "))
+    idade_dia = int(input("Digite aqui quantos dias voce tem: "))
+
+    ano = idade_ano*365
+    mes = idade_mes*30
+    soma = ano+mes+idade_dia
+    print("Sua idade em dias é aproximadamente", soma)
+
+    continua = input("Deseja repetir a aplicação? ").lower()
 
