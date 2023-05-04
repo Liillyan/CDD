0bae54829051ea66f8af3602091c6d891bfc22ef
Liillyan <lillyanr95@gmail.com>
2023-05-04 20:48:12 -0300

    Exercicio resolvido

--- Revisão_Logica/20-exercicio_10numeros.py
+++ Revisão_Logica/20-exercicio_10numeros.py
@@ -7,3 +7,24 @@ usuário 10 números e mostre:
 4. todos os números negativos;
 5. todos os zeros que aparecem no array'''
 
+A = []
+
+for x in range(10):
+    A.append(int(input("Digite um numero: ")))
+print(A)
+
+for y in range(10):
+    if A[y] % 2!=0:
+        print(f"impares: {A[y]}")
+for w in range(10):
+    if A[w] %2 ==0:
+        print(f"pares: {A[w]}")
+for k in range(10):
+    if A[k] >0:
+        print(f"numeros positivos: {A[k]}")
+for t in range(10):
+    if A[t] <0:
+        print(f"numeros negativos: {A[t]}")
+for u in range(10):
+    if A[u] ==0:
+        print(f"todos os zeros: {A[u]}")
