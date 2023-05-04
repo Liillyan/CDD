6686265269f4dbb9c220f2c1c6149fac73450a09
Lillyan <103754183+Liillyan@users.noreply.github.com>
2023-05-04 09:36:14 -0300

    Update exercicio_antecessor.py
    Na revisão em sala foi questionado a possibilidade do usuário digitar um numero negativo, e do modo feito anteriormente não teria um retorno satisfatório, o código foi alterado considerando números negativos.

--- exercicio_antecessor.py
+++ exercicio_antecessor.py
@@ -14,3 +14,24 @@ sucessor.'''
 
 num = int(input("Digite um numero e será printado seu sucessor: "))
 print(f" o sucessor de {num} é {(num+1)}")
+
+#CÓDIGO ACIMA FEITO EM CASA ENQUANTO QUE O CÓDIGO ABAIXO FOI REFEITO EM SALA#
+
+'''Durante a aula foi questionado que o usuario poderia digitar um numero negativo,
+então foi refeito o código'''
+
+n = int(input("Digite aqui um numero e direi seu antecessor: "))
+
+if n >= 0:
+    print(f"O antecessor de {n} é {n-1}")
+else:
+    print(f"O antecessor de {n} é {n-(-1)}")
+
+print("---------------------------------------------------------")
+
+num = int(input("Digite aqui um numero e direi seu sucessor: "))
+
+if num <= 0:
+    print(f"O sucessor de {num} é {num+1}")
+else:
+    print(f"O sucessor de {num} é {num - (+1)}")
