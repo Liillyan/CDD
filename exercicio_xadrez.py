3481e34b3a33c828858987bc6208f931d57c7628
Lillyan <103754183+Liillyan@users.noreply.github.com>
2023-05-04 12:18:14 -0300

    Update exercicio_xadrez.py

--- exercicio_xadrez.py
+++ exercicio_xadrez.py
@@ -12,3 +12,17 @@ A=[2,5,8,4,7,5,2]
 for x in range(6, -1, -1):
     B.append(A[x])
 #   B[6-x]=A[x]
+
+print("---------------------------------------------------------")
+
+cont_impar=0
+A=[]
+cont = 0
+
+while true
+    if cont%2!=0:
+        A.ppend(cont)
+        cont_impar+=1
+cont+=1
+if cont_impar ==10:
+    break
