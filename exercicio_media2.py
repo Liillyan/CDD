4e1732f43fcf9fef2f57d2f5b4505d03a4796301
Lillyan <103754183+Liillyan@users.noreply.github.com>
2023-05-04 08:23:50 -0300

    Update exercicio_media2.py
    Alteração utilizando o for para otimizar o código

--- exercicio_media2.py
+++ exercicio_media2.py
@@ -12,3 +12,12 @@ n4 = int(input("Digite o 4º numero: "))
 soma = (n1+n2+n3+n4)
 media = soma/4
 print(f"A media desses 4 numeros é {media}")
+
+#mesmo exercicio feito com for#
+
+soma = 0
+
+for x in range(1, 5):
+  num = int(input(f"Digite aqui o {[x]} numero: "))
+  soma=soma+num
+print(f" A media dos 4 numeros digitados é {soma/4})
