7c4073d05c274965869e703bfc3ded1fd667cd58
Lillyan <103754183+Liillyan@users.noreply.github.com>
2023-05-04 11:37:56 -0300

    Update exercicio_urna.py
    Código refatorado sendo adicionado o while para permitir que usuário possa reiniciar a aplicação

--- exercicio_urna.py
+++ exercicio_urna.py
@@ -17,3 +17,30 @@ porc_valido = (valido*100)/eleitores
 print(f"Desse total temos que {porc_branco}% dos eleitores votaram em branco,"
       f"{porc_nulo}% anularam seu voto, e por fim {porc_valido}% eleitores"
       f" votaram em algum candidato válido totalizando {(porc_valido+porc_nulo+porc_branco)}% das urnas apuradas")
+
+print("------------------------------------------------------------------------------------------------------------")
+
+#Código refatorado em sala de aula#
+
+continua = "sim"
+
+while continua == "sim":
+
+    quant_eleitores = int(input("Digite aqui quantos eleitores participaram dessa eleição: "))
+    votos_brancos = int(input("Digite aqui quantos eleitores votaram em branco nessa eleição: "))
+    votos_nulos = int(input("Digite aqui quantos eleitores votaram nulo nessa eleição: "))
+    votos_validos = int(input("Digite aqui quantos eleitores tiverem seus votos validos nessa eleição: "))
+
+    soma = votos_validos+votos_nulos+votos_brancos
+
+    if soma != quant_eleitores:
+        print("Voce digitou alguma quantidade errada")
+
+    else:
+        porc_votos_brancos = votos_brancos*100/quant_eleitores
+        porc_votos_nulos = votos_nulos * 100 / quant_eleitores
+        porc_votos_validos = votos_validos * 100 / quant_eleitores
+            
+        print(f"De {quant_eleitores} votos, {porc_votos_brancos}% foram votos em branco, {votos_nulos}% foram votos nulos e {porc_votos_validos}% foram votos válidos")
+
+    continua = input("Deseja fazer um novo calculo? ").lower()
